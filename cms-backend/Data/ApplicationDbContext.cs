0329467f2fa9e27ee54bc27ad2186299b3b110f6
sohag47 <minhazul.islamcse@gmail.com>
2025-05-14 00:05:08 +0600

    Add project files.

---
+++ cms-backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using cms_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cms_backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ cms-backend/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cms_backend.Models
+{
+    public class User
+    {
+        [Key]  // Primary Key
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
