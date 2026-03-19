b10d55f23742702172347f394fd8754041a3dffe
sohag47 <minhazul.islamcse@gmail.com>
2026-03-19 13:48:03 +0600

    everythingis ok

--- cms-backend/Enums/CategoryStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace cms_backend.Enums;
-
-public enum CategoryStatus
-{
-    Active = 1,
-    Inactive = 2,
-    Draft = 3,
-}
--- cms-backend/Models/Base/BaseEntity.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace cms_backend.Models.Base;
-
-public abstract class BaseEntity
-{
-    public int Id { get; set; }
-}
